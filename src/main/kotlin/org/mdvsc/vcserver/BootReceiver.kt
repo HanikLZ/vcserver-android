433bf4dcdf9664678712d5455d6c2c86b34f4a50
HanikLZ <haniklz@gmail.com>
2017-05-15 03:37:12 +0800

    fix auto start on boot option is not working

--- src/main/kotlin/org/mdvsc/vcserver/BootReceiver.kt
+++ src/main/kotlin/org/mdvsc/vcserver/BootReceiver.kt
@@ -18,37 +18,23 @@ class BootReceiver: BroadcastReceiver() {
         val preferences = MyApplication.applicationComponent.appPreferences()
         val startCommand = "start"
         val stopCommand = "stop"
-        val commandStr = when(intent.action) {
-            Intent.ACTION_BOOT_COMPLETED -> {
-                preferences.getAutoStartMethod() == "follow_system_boot"
-                startCommand
-            }
-            WifiManager.WIFI_STATE_CHANGED_ACTION -> {
-                if (preferences.getAutoStartMethod() == "when_wifi_on") {
-                    when (intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN)) {
-                        WifiManager.WIFI_STATE_ENABLED -> startCommand
-                        WifiManager.WIFI_STATE_DISABLED -> stopCommand
-                        else -> null
-                    }
-                } else {
-                    null
+        when(intent.action) {
+            Intent.ACTION_BOOT_COMPLETED -> if (preferences.getAutoStartMethod() == "follow_system_boot") startCommand else null
+            WifiManager.WIFI_STATE_CHANGED_ACTION -> if (preferences.getAutoStartMethod() == "when_wifi_on") {
+                when (intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN)) {
+                    WifiManager.WIFI_STATE_ENABLED -> startCommand
+                    WifiManager.WIFI_STATE_DISABLED -> stopCommand
+                    else -> null
                 }
-            }
-            "android.net.wifi.WIFI_AP_STATE_CHANGED" -> {
-                if (preferences.getAutoStartMethod() == "when_wifi_hotspot") {
-                    when(intent.getIntExtra("wifi_state", 0)) {
-                        13 -> startCommand
-                        11 -> stopCommand
-                        else -> null
-                    }
-                } else {
-                    null
+            } else null
+            "android.net.wifi.WIFI_AP_STATE_CHANGED" -> if (preferences.getAutoStartMethod() == "when_wifi_hotspot") {
+                when(intent.getIntExtra("wifi_state", 0)) {
+                    13 -> startCommand
+                    11 -> stopCommand
+                    else -> null
                 }
-            }
+            } else null
             else -> null
-        }
-        if (commandStr != null) {
-            context.startService(Intent(context, ServerService::class.java).putExtra("command", commandStr))
-        }
+        }?.let { context.startService(Intent(context, ServerService::class.java).putExtra("command", it)) }
     }
 }
